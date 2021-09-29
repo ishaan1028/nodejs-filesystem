f65efcb9f84b1aeaaf48c40f28da9e89e1e2563b
ishaan1028 <ishaandhawane77@gmail.com>
2021-09-29 19:13:09 +0530

    add path

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const { fetchRouter } = require("./routes/fetch.route");
 
 const express = require("express");
 const fs = require("fs");
-const path = require('path');
-
-
 
 const app = express();
 const port = 3001;
