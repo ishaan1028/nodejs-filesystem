aa3a7253cd8fe665e043c5f4300f207134f7ea0c
ishaan1028 <ishaandhawane77@gmail.com>
2021-09-29 18:56:20 +0530

    add path

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const { fetchRouter } = require("./routes/fetch.route");
 
 const express = require("express");
 const fs = require("fs");
+const path = require('path');
+
+
 
 const app = express();
 const port = 3001;
 const dir = "D:/filesystem";
+
+const dirnamePath = path.join(__dirname, '/picturesdirname');
+const dirPath = path.join(dir, '/picturesdir');
 
 // Middlewares
 app.use("/create", createRouter);
@@ -18,6 +24,9 @@ app.get("/", (req, res) => {
 
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir, { recursive: true });
+        fs.mkdirSync(dirnamePath);
+        fs.mkdirSync(dirPath);
+
     }
     res.send("check directory filesystem created at D drive! use /create to create and /fetch to fetch files!");
 });
